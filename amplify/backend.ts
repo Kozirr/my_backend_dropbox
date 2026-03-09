a26653198c27231273922871959ac597c1ae2a56
Kozirr <qurbanovnuraddin7@gmail.com>
2026-03-09 21:28:34 +0400

    fix: order stream policies before event mappings

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -37,33 +37,33 @@ renameFn.addEnvironment("FILERECORD_TABLE_NAME", tableName);
 
 const lambdaStack = backend.createStack("LambdaPermissions");
 
-deleteFn.role!.attachInlinePolicy(
-  new Policy(lambdaStack, "DeleteS3Policy", {
-    statements: [
-      new PolicyStatement({
-        actions: ["s3:DeleteObject"],
-        resources: [backend.storage.resources.bucket.arnForObjects("*")],
-      }),
-    ],
-  })
-);
+const deleteS3Policy = new Policy(lambdaStack, "DeleteS3Policy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["s3:DeleteObject"],
+      resources: [backend.storage.resources.bucket.arnForObjects("*")],
+    }),
+  ],
+});
 
-renameFn.role!.attachInlinePolicy(
-  new Policy(lambdaStack, "RenameS3DynamoPolicy", {
-    statements: [
-      new PolicyStatement({
-        actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-        resources: [backend.storage.resources.bucket.arnForObjects("*")],
-      }),
-      new PolicyStatement({
-        actions: ["dynamodb:UpdateItem"],
-        resources: [fileRecordTable.tableArn],
-      }),
-    ],
-  })
-);
+deleteFn.role!.attachInlinePolicy(deleteS3Policy);
 
-new EventSourceMapping(lambdaStack, "DeleteRecordStream", {
+const renameS3DynamoPolicy = new Policy(lambdaStack, "RenameS3DynamoPolicy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+      resources: [backend.storage.resources.bucket.arnForObjects("*")],
+    }),
+    new PolicyStatement({
+      actions: ["dynamodb:UpdateItem"],
+      resources: [fileRecordTable.tableArn],
+    }),
+  ],
+});
+
+renameFn.role!.attachInlinePolicy(renameS3DynamoPolicy);
+
+const deleteRecordStream = new EventSourceMapping(lambdaStack, "DeleteRecordStream", {
   target: deleteFn,
   eventSourceArn: tableStreamArn,
   startingPosition: StartingPosition.LATEST,
@@ -71,7 +71,7 @@ new EventSourceMapping(lambdaStack, "DeleteRecordStream", {
   enabled: true,
 });
 
-new EventSourceMapping(lambdaStack, "RenameFileStream", {
+const renameFileStream = new EventSourceMapping(lambdaStack, "RenameFileStream", {
   target: renameFn,
   eventSourceArn: tableStreamArn,
   startingPosition: StartingPosition.LATEST,
@@ -89,14 +89,17 @@ const streamPolicy = new PolicyStatement({
   resources: [tableStreamArn],
 });
 
-deleteFn.role!.attachInlinePolicy(
-  new Policy(lambdaStack, "DeleteStreamReadPolicy", {
-    statements: [streamPolicy],
-  })
-);
+const deleteStreamReadPolicy = new Policy(lambdaStack, "DeleteStreamReadPolicy", {
+  statements: [streamPolicy],
+});
 
-renameFn.role!.attachInlinePolicy(
-  new Policy(lambdaStack, "RenameStreamReadPolicy", {
-    statements: [streamPolicy],
-  })
-);
+deleteFn.role!.attachInlinePolicy(deleteStreamReadPolicy);
+
+const renameStreamReadPolicy = new Policy(lambdaStack, "RenameStreamReadPolicy", {
+  statements: [streamPolicy],
+});
+
+renameFn.role!.attachInlinePolicy(renameStreamReadPolicy);
+
+deleteRecordStream.node.addDependency(deleteStreamReadPolicy);
+renameFileStream.node.addDependency(renameStreamReadPolicy);
