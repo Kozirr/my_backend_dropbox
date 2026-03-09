761f1c10796b4a90bb5532dd91d97e9903028f1d
Kozirr <qurbanovnuraddin7@gmail.com>
2026-03-09 19:57:11 +0400

    fix: isolate Python handlers into subdirectories to prevent Docker bundling

--- amplify/functions/onRenameFile/resource.ts
+++ amplify/functions/onRenameFile/resource.ts
@@ -1,32 +1,19 @@
 import * as path from "path";
 import { Runtime, Code, Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
-import { Stack } from "aws-cdk-lib";
-import { BundlingOutput } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export function createOnRenameFileFunction(scope: Stack, id: string) {
+export function createOnRenameFileFunction(scope: Construct, id: string) {
+  const codePath = path.resolve(
+    process.cwd(),
+    "amplify",
+    "functions",
+    "onRenameFile",
+    "handler"
+  );
   return new LambdaFunction(scope, id, {
     runtime: Runtime.PYTHON_3_12,
     handler: "handler.handler",
-    code: Code.fromAsset(path.join(__dirname), {
-      bundling: {
-        image: Runtime.PYTHON_3_12.bundlingImage,
-        command: ["bash", "-c", "cp -r . /asset-output"],
-        outputType: BundlingOutput.NOT_ARCHIVED,
-        local: {
-          tryBundle(outputDir: string) {
-            const fs = require("fs");
-            const p = require("path");
-            const srcDir = path.join(__dirname);
-            for (const file of fs.readdirSync(srcDir)) {
-              if (file.endsWith(".py")) {
-                fs.copyFileSync(p.join(srcDir, file), p.join(outputDir, file));
-              }
-            }
-            return true;
-          },
-        },
-      },
-    }),
+    code: Code.fromAsset(codePath),
     functionName: `onRenameFile-${id}`,
   });
 }
