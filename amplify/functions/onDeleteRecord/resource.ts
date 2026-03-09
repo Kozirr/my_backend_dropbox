306e4088fbb2f48c2de6e2949bf61ca3a588244f
Kozirr <qurbanovnuraddin7@gmail.com>
2026-03-09 20:04:51 +0400

    fix: use Code.fromInline for Python Lambdas to eliminate Docker dependency

--- amplify/functions/onDeleteRecord/resource.ts
+++ amplify/functions/onDeleteRecord/resource.ts
@@ -1,19 +1,17 @@
+import * as fs from "fs";
 import * as path from "path";
 import { Runtime, Code, Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export function createOnDeleteRecordFunction(scope: Construct, id: string) {
-  const codePath = path.resolve(
-    process.cwd(),
-    "amplify",
-    "functions",
-    "onDeleteRecord",
-    "handler"
+  const handlerCode = fs.readFileSync(
+    path.join(__dirname, "handler", "handler.py"),
+    "utf-8"
   );
   return new LambdaFunction(scope, id, {
     runtime: Runtime.PYTHON_3_12,
-    handler: "handler.handler",
-    code: Code.fromAsset(codePath),
+    handler: "index.handler",
+    code: Code.fromInline(handlerCode),
     functionName: `onDeleteRecord-${id}`,
   });
 }
